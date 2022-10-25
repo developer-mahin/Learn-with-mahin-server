2604f26d5c724f6905a3c195e74f6b8e1405605b
developer-mahin <mdmahin1310@gmail.com>
2022-10-25 22:03:09 +0600

    create course server api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require("cors")
 const port = process.env.PORT || 5000
+const courses = require("./data/courses.json")
 
 app.use(cors())
 
@@ -10,8 +11,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/', (req, res)=>{
-    
+app.get('/courses', (req, res)=>{
+    res.send(courses)
 })
 
 app.listen(port, () => {
